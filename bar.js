4d0984969d84e483c09bf24584129b89069abace
Jyri-Matti Lähteenmäki <jyri-matti@lahteenmaki.net>
2012-11-16 15:36:15 +0200

    hoveria ja onkeytä

--- bar.js
+++ bar.js
@@ -7,9 +7,33 @@ $(document).ready(function() {
 		}, 0) / months.length;
 	});
 
-	var bars = $('body').prepend('<div class="yearBars"></div>')
+	var bars = $('body').prepend('<div id="yearBars" class="yearBars"></div>')
 			 .find('.yearBars');
 	_.each(_.zip(years, averageYearTemperatures), function(arr) {
-		bars.append('<div class="yearBar" title="Vuosi ' + arr[0] + '" style="height: ' + (arr[1]) + 'em; margin-top: ' + (20-arr[1]) + 'em"><div class="year">' + arr[0] + '</div><div class="temp">' + arr[1] + '</div></div>');
+		bars.append('<div class="yearBar" title="Vuosi ' + arr[0] + '" style="height: ' + (arr[1]) + 'em; margin-top: ' + (15-arr[1]) + 'em"><div class="year">' + arr[0] + '</div><div class="temp">' + ('' + arr[1]).substring(0, 4) + ' °C</div></div>');
+	});
+
+	$('body').keydown(function(event) {
+		if (event.keyCode == 37) {
+			var selected = $('#yearBars .selected');
+			if (selected.length == 0 || $('#yearBars .yearBar').first().filter('.selected').length > 0) {
+				selected.removeClass('selected');
+				$('#yearBars .yearBar').last().addClass('selected');
+			} else {
+				var sel = $('.selected').prev().last();
+				selected.removeClass('selected');
+				sel.addClass('selected');
+			}
+		} else if (event.keyCode == 39) {
+			var selected = $('#yearBars .selected');
+			if (selected.length == 0 || $('#yearBars .yearBar').last().filter('.selected').length > 0) {
+				selected.removeClass('selected');
+				$('#yearBars .yearBar').first().addClass('selected');
+			} else {
+				var sel = $('.selected').next().first();
+				selected.removeClass('selected');
+				sel.addClass('selected');
+			}
+		}
 	});
 });
